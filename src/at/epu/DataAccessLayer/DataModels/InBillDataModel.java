f5dfd577c2dc94cad4ec24e845da06dd513af64a
benny99 <b.s80@gmx.at>
2012-03-18 14:49:42 +0100

    added DB SCRIPTS! view README for instructions

--- src/at/epu/DataAccessLayer/DataModels/InBillDataModel.java
+++ src/at/epu/DataAccessLayer/DataModels/InBillDataModel.java
@@ -4,7 +4,7 @@ public abstract class InBillDataModel extends BackofficeTableModel {
 	private static final long serialVersionUID = -8915600980699424021L;
 	
 	public InBillDataModel() {
-		String [] columnNames_ = {"Ausgangsrechnung ID",
+		String [] columnNames_ = {"Eingangsrechnung ID",
 				"Kontakt",
 				"Anz Rechnungszeilen",
 				"Anz Buchungszeilen",
