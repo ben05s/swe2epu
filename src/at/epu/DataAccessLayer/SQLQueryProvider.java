1af7f6265ccc76b910d16740a23f3aa3fbc8c568
benny99 <b.s80@gmx.at>
2012-03-20 23:14:03 +0100

    "minor fix"

--- src/at/epu/DataAccessLayer/SQLQueryProvider.java
+++ src/at/epu/DataAccessLayer/SQLQueryProvider.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-
 import at.epu.DataAccessLayer.DataModels.BackofficeTableModel;
 import at.epu.DataAccessLayer.DataObjects.DataObject;
 
