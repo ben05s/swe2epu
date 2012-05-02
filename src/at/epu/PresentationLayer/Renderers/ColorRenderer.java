294d3aa86d351278f4c407a5b6bf5b60c8256dee
Patrick Schwab <patrick.schwab@technikum-wien.at>
2012-05-02 14:03:32 +0200

    fixed edit/add VC bugs

---
+++ src/at/epu/PresentationLayer/Renderers/ColorRenderer.java
@@ -0,0 +1,42 @@
+package at.epu.PresentationLayer.Renderers;
+
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+public class ColorRenderer extends JLabel implements TableCellRenderer {
+
+	public ColorRenderer(String column) {
+		super(column);
+		setOpaque(true);
+	}
+
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value,
+			boolean isSelected, boolean hasFocus, int row, int column) {
+		
+        if (value != null) {
+        	setText(value.toString());
+        }
+        
+        if(isSelected)
+        {
+        	setBackground(table.getSelectionBackground());
+        	setForeground(table.getSelectionForeground());
+        }
+        else
+        {
+        	setBackground(table.getBackground());
+        	setForeground(table.getForeground());
+        
+        	if(row % 2 == 0) {
+        		setBackground(new Color(242, 242, 255));
+        	}
+        }
+        
+		return this;
+	}
+
+}
---
+++ tests/at/epu/test/suites/DataObjectTest.java
@@ -0,0 +1,47 @@
+package at.epu.test.suites;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import at.epu.DataAccessLayer.DataObjects.ContactDataObject;
+
+public class DataObjectTest {
+
+	@Test
+	public void testGetFieldNames() {
+		ContactDataObject contact = new ContactDataObject();
+		
+		ArrayList<String> fieldNames = contact.getFieldNames();
+		
+		assertEquals("id", fieldNames.get(0));
+		assertEquals("vorname", fieldNames.get(1));
+		assertEquals("nachname", fieldNames.get(2));
+		assertEquals("adresse", fieldNames.get(3));
+		assertEquals("email", fieldNames.get(4));
+		assertEquals("telefon", fieldNames.get(5));
+	}
+
+	@Test
+	public void testGetFieldValues() {
+		ContactDataObject contact = new ContactDataObject();
+		
+		contact.setId(0);
+		contact.setVorname("Herbert");
+		contact.setNachname("Foobar");
+		contact.setEmail("herbi@foobi.at");
+		contact.setAdresse("herbifoobigasse 7/123");
+		contact.setTelefon("066666666");
+		
+		ArrayList<Object> fieldValues = contact.getFieldValues();
+		
+		assertEquals("0", fieldValues.get(0).toString());
+		assertEquals("Herbert", fieldValues.get(1).toString());
+		assertEquals("Foobar", fieldValues.get(2).toString());
+		assertEquals("herbifoobigasse 7/123", fieldValues.get(3).toString());
+		assertEquals("herbi@foobi.at", fieldValues.get(4).toString());
+		assertEquals("066666666", fieldValues.get(5).toString());
+	}
+}
