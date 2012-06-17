237168924011dbd1009822441861a56481792a2d
Patrick Schwab <patrick.schwab@technikum-wien.at>
2012-06-17 22:15:08 +0200

    configuration files implemented

--- src/at/epu/BusinessLayer/ApplicationManager.java
+++ src/at/epu/BusinessLayer/ApplicationManager.java
@@ -1,5 +1,10 @@
 package at.epu.BusinessLayer;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
@@ -41,20 +46,51 @@ public class ApplicationManager {
 	 */
 	public void applicationStarted(String[] args) {	
 		
-		PropertyConfigurator.configure("config/logging.properties");
-		
-		if(args.length > 0)
-		{
-			/** SQL */
-			databaseManager = new DatabaseManager(args[0]);
+		if(args.length > 0) {
+			Properties appConfig = new Properties();
+			
+			try {
+				appConfig.load(new FileInputStream(args[0]));
+			} catch (FileNotFoundException e) {
+				System.err.println("Could not find the application configuration file at config/app.properties.");
+			} catch (IOException e) {
+				System.err.println("Could not open the application configuration file at config/app.properties.");
+			}
+			
+			String loggingConfig = appConfig.getProperty("logging.config.filepath");
+			
+			if(loggingConfig != null) {
+				PropertyConfigurator.configure(loggingConfig);
+			} else {
+				System.err.println("To use the logging framework u must specify a value for the key logging.config.filepath in your app.properties file.");
+			}
+			
+			String databaseString = appConfig.getProperty("database.path");
+			
+			if(databaseString != null)
+			{
+				/** SQL */
+				databaseManager = new DatabaseManager(databaseString);
+			}
+			else
+			{
+				/** Mock */
+				databaseManager = new DatabaseManager();
+			}
+			
+			Logger.getLogger(ApplicationManager.class.getName()).info("Read application configuration file at " + args[0] + ".");
+			
+			if(databaseString != null) {
+				Logger.getLogger(ApplicationManager.class.getName()).info("Database string is: " + databaseString + ".");
+			} else {
+				Logger.getLogger(ApplicationManager.class.getName()).info("Using the mock database.");
+			}
+			
+			Logger.getLogger(ApplicationManager.class.getName()).info("Logging configuration is at: " + loggingConfig + ".");
+			Logger.getLogger(ApplicationManager.class.getName()).info("Application started.");
+		} else {
+			System.err.println("You must supply an application configuration file at startup. \nUsage: ./backofficedb [APP/CONFIG/FILEPATH.properties]");
 		}
-		else
-		{
-			/** Mock */
-			databaseManager = new DatabaseManager();
-		}
-		
-		Logger.getLogger(ApplicationManager.class.getName()).info("Application started.");
 	}
 	
 	/**
